aaf83a24aa8cc23efdcc1510d91a1ed9c510554f
mtso <mtso@mail.com>
2016-10-24 00:47:31 -0700

    Add comments:

--- StackADT/List.cpp
+++ StackADT/List.cpp
@@ -1,4 +1,13 @@
+// List.cpp
+// Lab 2: Stack ADT
+// CIS 22C F2016: Adrian Marroquin, Matthew Tso
+
+#ifndef STACKADT_LIST_CPP
+#define STACKADT_LIST_CPP
+
 #include "List.h"
 #include "Node.h"
 
-template <typename DataType>
+// template <typename DataType>
+
+#endif
--- StackADT/List.h
+++ StackADT/List.h
@@ -1,5 +1,16 @@
+// Node.h
+// Lab 2: Stack ADT
+// CIS 22C F2016: Adrian Marroquin, Matthew Tso
+
+#ifndef STACKADT_LIST_H
+#define STACKADT_LIST_H
+
 #include "Node.h"
 
+/**
+ * Linked List implementation of Bag ADT
+ *
+ */
 template <typename DataType>
 class List 
 {
@@ -10,8 +21,14 @@ public:
 	List();
 	~List(); 
 	bool isEmpty(int position);
-	bool insert(int position, DataType itemn);
+	bool insert(int position, DataType item);
 	bool remove(int position);
 	Node<DataType> getItem(int position);
 	void setItem(int position);
+
+	// TODO: add clear
+	void clear();
 };
+
+//#include "List.cpp"
+#endif
