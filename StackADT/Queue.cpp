55b2af032fe954dae0e4aa96b88c47649fa2acfe
mtso <mtso@mail.com>
2016-10-26 23:04:45 -0700

    Add Queue class declaration

---
+++ StackADT/Queue.cpp
@@ -0,0 +1,12 @@
+// Queue.cpp
+// Lab 2b: Queue ADT
+// CIS 22C F2016: Adrian Marroquin, Matthew Tso
+
+#ifndef STACKADT_QUEUE_CPP
+#define STACKADT_QUEUE_CPP
+
+#include "Queue.h"
+
+// T_DATA bool Queue<DataType>::isEmpty() {}
+
+#endif
