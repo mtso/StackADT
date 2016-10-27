1a914ca5c86e1a751ad067dfb0c028a8970a7d64
mtso <mtso@mail.com>
2016-10-26 23:23:11 -0700

    Add QueueDestructor test

--- StackADTTests/QueueTests.cpp
+++ StackADTTests/QueueTests.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "Queue.h"
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -18,6 +19,30 @@ namespace StackADTTests
 			//queue.enqueue(2);
 
 			Assert::Fail();
+		}
+
+		TEST_METHOD(QueueDestructor)
+		{
+			Queue<int>* queue = new Queue<int>();
+			delete queue;
+
+			ifstream log("stackadt_log.txt");
+			if (log.is_open())
+			{
+				vector<string> reversedLog;
+				string line;
+
+				while (getline(log, line))
+				{
+					reversedLog.insert(reversedLog.begin(), line);
+				}
+
+				string listLog = util::removeTimestamp(reversedLog[0]);
+				string queueLog = util::removeTimestamp(reversedLog[1]);
+
+				Assert::AreEqual(string("~List called"), listLog);
+				Assert::AreEqual(string("~Queue called"), queueLog);
+			}
 		}
 	};
 }
